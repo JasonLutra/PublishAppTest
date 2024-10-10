be5d585cdb2cf67efb94bfcacbb160ae41081292
JasonLutra <jason.firth@lutra.com>
2024-10-11 11:41:20 +1300

    added prompt to program

--- PublishAppTest/Program.cs
+++ PublishAppTest/Program.cs
@@ -3,5 +3,12 @@ Console.WriteLine("Hello, World!");
 
 Console.WriteLine("This is a new line");
 
-string input = Console.Readline("Please input your data");
+Console.WriteLine("Please input your data:");
 
+string input = Console.ReadLine();
+
+Console.WriteLine("your input was: " + input);
+
+Console.WriteLine("Press enter to exit");
+
+string input2 = Console.ReadLine();
