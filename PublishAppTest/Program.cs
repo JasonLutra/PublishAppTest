56d46b6c3e6d229e173e1abe6a2ed9004e05562a
JasonLutra <jason.firth@lutra.com>
2024-10-14 09:47:16 +1300

    added comments to the program file

--- PublishAppTest/Program.cs
+++ PublishAppTest/Program.cs
@@ -1,14 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
-
+//test line
 Console.WriteLine("This is a new line");
 
 Console.WriteLine("Please input your data:");
-
+//read users input and store in variable
 string input = Console.ReadLine();
 
 Console.WriteLine("your input was: " + input);
-
+//this is stop the window from closing
 Console.WriteLine("Press enter to exit");
 
 string input2 = Console.ReadLine();
